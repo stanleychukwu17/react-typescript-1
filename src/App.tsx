386f8fc2bf9ecfe74a4f712d5032cf72e501c861
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-05-16 18:45:25 +0100

    cleaned up! so i can add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import GreetProps from './GreetComp'
-import ButtonComp from './ButtonComp'
 
 function App() {
-  const dts = {'firstName':'stanely', 'lastName':'chukwu', 'age':26}
+  const dts: object = {'firstName':'stanely', 'lastName':'chukwu', 'age':26}
+
+  console.log(dts)
   return (
     <div className="App">
-      <GreetProps info={dts} />
-      <ButtonComp handleClick={(event) => {
-        console.log('now we done it', event.target)
-        return 15;
-      }} />
+
     </div>
   );
 }
