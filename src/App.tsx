1821510b9afdfd3ec8ebcf0dfa376d7838ab906a
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-05-16 20:46:10 +0100

    Added 2 new components to round up the type props section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
 
+// the below are all from lecture-5 - Advanced props
 import Status from './Status';
+import Heading from './Heading';
+import Oscar from './Oscar';
 
 function App() {
   const dts: object = {'firstName':'stanely', 'lastName':'chukwu', 'age':26}
-
   console.log(dts)
+
   return (
     <div className="App">
-      <Status msg='success' />
+      <div>
+        <h1>Lecture 5</h1>
+        <Status msg='success' />
+        <Heading>This is the children testing props</Heading>
+        <Oscar>
+          <Heading>Showing in the oscars</Heading>
+        </Oscar>
+      </div>
     </div>
   );
 }
---
+++ src/Oscar.tsx
@@ -0,0 +1,9 @@
+type oscarProps = {
+    children: React.ReactNode
+}
+
+export default function Oscar(props: oscarProps) {
+  return (
+    <div>{props.children}</div>
+  )
+}
