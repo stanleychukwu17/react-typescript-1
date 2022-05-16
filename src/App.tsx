1e919a446b4644ea1ccdbe82971eca00c2bc2fb9
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-05-16 18:54:40 +0100

    Added a new component status and imported into the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+import Status from './Status';
 
 function App() {
   const dts: object = {'firstName':'stanely', 'lastName':'chukwu', 'age':26}
@@ -8,7 +9,7 @@ function App() {
   console.log(dts)
   return (
     <div className="App">
-
+      <Status />
     </div>
   );
 }
---
+++ src/Status.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Status() {
+  return (
+    <div>
+        <h2>Loading...</h2>
+        <h2>Data has been fetched</h2>
+        <h2>Error fetching data</h2>
+    </div>
+  )
+}
