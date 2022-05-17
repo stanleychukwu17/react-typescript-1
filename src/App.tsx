affe365d70818b58b6c99ca4f4b76802b69c6bd6
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-05-17 09:49:43 +0100

    working on lecture 7, this one deals with styling props

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ import Status from './components/5/Status';
 import Heading from './components/5/Heading';
 import Oscar from './components/5/Oscar';
 
-// the below are all from lecture-6 - Event props
+// the below are all from lecture-6 - Event props & lecture-7 style props
 import Button from './components/6/Button';
 import Input from './components/6/Input';
+import Container from './components/7/Container';
 
 function App() {
   const dts: object = {'firstName':'stanely', 'lastName':'chukwu', 'age':26}
@@ -16,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      <div className='cover'>
         <h1>Lecture 5</h1>
         <Status msg='success' />
         <Heading>This is the children testing props</Heading>
@@ -25,13 +26,28 @@ function App() {
         </Oscar>
       </div>
 
-      <div>
+      <div className='cover'>
         <h1>Lecture 6</h1>
         <Button clickHandler={(event, id) => { console.log('clicked me', event, id) }} />
         <Input
           value='james'
           changeHandler={(event) => { console.log('changed me', event, event.target.value) }}
         />
+      </div>
+
+      <div className='cover'>
+        <h1>Lecture 7</h1>
+        <Container />
+      </div>
+
+
+
+      <div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
       </div>
     </div>
   );
---
+++ src/components/7/Container.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Container() {
+  return (
+    <div style={{color: '#df0e3a', fontWeight: 'bold'}}>
+        This is from stanley the man
+    </div>
+  )
+}
