2b92f24fea8f5523b0d083c29c2d53776b2c9392
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-08 16:26:12 +0100

    done with lecture 22

--- src/components/22/Button.tsx
+++ src/components/22/Button.tsx
@@ -1,8 +1,7 @@
+// E.g 1
 type ButtonProps = {
     variant: 'primary' | 'secondary',
 } & React.ComponentProps<'button'>
-
-type InputProps = React.ComponentProps<'input'>
 
 export default function CustomButton({variant, children, ...rest}: ButtonProps) {
     return (
@@ -12,6 +11,20 @@ export default function CustomButton({variant, children, ...rest}: ButtonProps) 
     )
 }
 
+
+// E.g 2
+type InputProps = React.ComponentProps<'input'>
+
 export const CustomInput = (props: InputProps) => {
     return <input {...props} />
 }
+
+/**
+the issue with the example above is that we can pass in <div> or <img /> as children for the custom button, we can tell typescript to accept only string. take a look at E.g 2
+E.g 2
+*/
+type ButtonProps2 = {
+    variant: 'primary' | 'secondary',
+    children: string
+} & Omit<React.ComponentProps<'button'>, 'children'>
+
